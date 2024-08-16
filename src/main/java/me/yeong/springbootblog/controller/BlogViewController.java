96b1ff656db2ab1f8dc8c22c9110e0032d28ec80
Geunyeong Yu <geunyeong0211@gmail.com>
2024-08-16 23:56:23 +0900

    Generate article view java [entity]Article created_at,updated_at 속성 추가 [application]SpringBootBlogApplication @EnableJpaAuditing 어노테이션 추가 [dto]ArticleViewResponse 생성 [controller]BlogViewController getArticle 메서드 생성 resource [data.sql] created_at,updated_at 속성 추가 [template]article.html 생성, articleList.html 버튼 수정

--- src/main/java/me/yeong/springbootblog/controller/BlogViewController.java
+++ src/main/java/me/yeong/springbootblog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.yeong.springbootblog.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.yeong.springbootblog.domain.Article;
 import me.yeong.springbootblog.dto.ArticleListViewResponse;
+import me.yeong.springbootblog.dto.ArticleViewResponse;
 import me.yeong.springbootblog.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +25,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); //블로그 글 리스트 저장
 
         return "articleList"; //articleList.html 이라는 이름의 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
---
+++ src/main/java/me/yeong/springbootblog/dto/ArticleViewResponse.java
@@ -0,0 +1,23 @@
+package me.yeong.springbootblog.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.yeong.springbootblog.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article){
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
